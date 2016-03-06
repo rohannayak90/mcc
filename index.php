ccdf772a16b1077c89b0edd304e1e3575a2b9500
rohannayak90 <rohannayak90@gmail.com>
2016-03-07 04:07:19 +0530

    Firs Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?> 
+        <button name="SUBMIT">
+            <a href="register.php"/>
+        </button>
+    </body>
+</html>
