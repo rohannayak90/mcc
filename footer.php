09d4bbc572a113b5cb6bea808ab03eff6de2a49c
rohannayak90 <rohannayak90@gmail.com>
2016-03-09 13:46:46 +0530

    Centralizing things

--- footer.php
+++ footer.php
@@ -2,29 +2,29 @@
             <div class="container">
                 <div id="footer-info">
                     <p>Copyright 2016 Creative Customizer. All rights reserved.</p>
-                    <p><a href="#">Terms of Service</a> | <a href="#">Privacy</a></p>
+                    <p><a href="javascript:void(0)">Terms of Service</a> | <a href="javascript:void(0)">Privacy</a></p>
                 </div>
                 <div id="footer-links">
                     <ul>
                         <li><h5>Company</h5></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Meet The Team</a></li>
-                        <li><a href="#">What We Do</a></li>
-                        <li><a href="#">Careers</a></li>
+                        <li><a href="javascript:void(0)">About Us</a></li>
+                        <li><a href="javascript:void(0)">Meet The Team</a></li>
+                        <li><a href="javascript:void(0)">What We Do</a></li>
+                        <li><a href="javascript:void(0)">Careers</a></li>
                     </ul>
                     <ul>
                         <li><h5>Company</h5></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Meet The Team</a></li>
-                        <li><a href="#">What We Do</a></li>
-                        <li><a href="#">Careers</a></li>
+                        <li><a href="javascript:void(0)">About Us</a></li>
+                        <li><a href="javascript:void(0)">Meet The Team</a></li>
+                        <li><a href="javascript:void(0)">What We Do</a></li>
+                        <li><a href="javascript:void(0)">Careers</a></li>
                     </ul>
                     <ul>
                         <li><h5>Company</h5></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Meet The Team</a></li>
-                        <li><a href="#">What We Do</a></li>
-                        <li><a href="#">Careers</a></li>
+                        <li><a href="javascript:void(0)">About Us</a></li>
+                        <li><a href="javascript:void(0)">Meet The Team</a></li>
+                        <li><a href="javascript:void(0)">What We Do</a></li>
+                        <li><a href="javascript:void(0)">Careers</a></li>
                     </ul>
                 </div>
                 <div class="clear"></div>
--- index.php
+++ index.php
@@ -15,11 +15,11 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis turpis ac libero interdum, id fringilla purus feugiat. Etiam malesuada mattis nibh at bibendum.</p>
                     </li>
                     <li class="feature-2">
-                        <h4>Business Cards</h4>
+                        <h4>Business</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis turpis ac libero interdum, id fringilla purus feugiat. Etiam malesuada mattis nibh at bibendum.</p>
                     </li>
                     <li class="feature-3">
-                        <h4>Marketing Materials</h4>
+                        <h4>Materials</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis turpis ac libero interdum, id fringilla purus feugiat. Etiam malesuada mattis nibh at bibendum.</p>
                     </li>
                     <div class="clear"></div>
@@ -32,27 +32,31 @@
                 <article>
                     <h3>Featured Content</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod leo a nibh dignissim tincidunt. Nam ultricies odio ac neque suscipit volutpat. Ut dictum adipiscing felis sed malesuada. Integer porta sem nec nibh hendrerit imperdiet. </p>
-                    <a href="#"><img src="images/video-placeholder.jpg" alt="video placeholder" /></a>
+                    <a href="javascript:void(0)"><img src="images/video-placeholder.jpg" alt="video placeholder" /></a>
                 </article>
             </div>
         </div>
         
         <div id="secondary-content">
             <div class="container">
-                <article style="background-image: url('images/article-image-1.jpg');">
-                    <div class="overlay">
-                        <h4>Secondary Content</h4>
-                        <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod leo a nibh dignissim tincidunt nam.</small></p>
-                        <a href="#" class="more-link">View more</a>
-                    </div>
-                </article>
-                <article style="background-image: url('images/article-image-2.jpg');">
-                    <div class="overlay">
-                        <h4>Secondary Content</h4>
-                        <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod leo a nibh dignissim tincidunt nam.</small></p>
-                        <a href="#" class="more-link">View more</a>
-                    </div>
-                </article>
+				<div class="row">
+					<article class="col-sm-6">
+						<img src="images/article-image-1.jpg" alt="" />
+						<div class="overlay">
+							<h4>Secondary Content</h4>
+							<p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod leo a nibh dignissim tincidunt nam.</small></p>
+							<a href="javascript:void(0)" class="more-link">View more</a>
+						</div>
+					</article>
+					<article class="col-sm-6">
+						<img src="images/article-image-2.jpg" alt="" />
+						<div class="overlay">
+							<h4>Secondary Content</h4>
+							<p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod leo a nibh dignissim tincidunt nam.</small></p>
+							<a href="javascript:void(0)" class="more-link">View more</a>
+						</div>
+					</article>
+				</div>
                 <div class="clear"></div>
             </div>
         </div>
